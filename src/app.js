4e58f59d7e12fa2b00ff1900ca6e61005aad67d0
Ivan <nquisition@gmail.com>
2018-09-03 23:34:19 +0900

    Bugfix

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ const bodyParser = require("body-parser");
 const guildRoutes = require("./routes/guild");
 const characterRoutes = require("./routes/character");
 
+const { guildName, guildRealm } = config;
+
 const app = express();
 
 const port = process.env.PORT;
